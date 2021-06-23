23ccaaf8c57bd27a510bfd633c769d2bed1712a8
Conor Paxton <conor.paxton@microchip.com>
2021-06-23 17:55:23 +0100

    Squashed 'driver-examples/mss-gpio/' changes from e1aab14c..0a262f45
    0a262f45 mpfs_configuration_generator.py: using the latest one, v0.6.3
    60aa70a5 .cproject: include path changes based on previous 2 commits.
    2abffe50 platform_config_ddr: Creating this new config to use with DDR_Release build.
    cad430cc platform_config: Since the TICK_RATE_MS is modified, creating a custom config_config. Using this for all build configs except DDR_Release.
    e7da127d u54_1.c: Correcting the hartid check
    9a12c7fb  changed default setting MPFS_HAL_LAST_HART = 4
    224d65c1 Removed .settings folder and its contents
    b3dee91d README.md formatting update
    5d853bfc  updated readme along with other minor changes
    9dbfe65d  Uart0 message transferd to e51 to create a default delay so that u54_1 can catch the software interrupt
    754d275f  Readme change
    65ecd5c6  minor fixes
    01249b25  changed debug config names and updated readme file to point towards the top level readme file .
    018abd49  updated lastest mpfs-hal , updated build config debug options to default (-g) for release configurations
    0b6cc4e7  Putting u54_1 in WFI in case of external bootloader not present
    c1bfc3d0 - updates-: 1. fixed things mentioned in PR review comments. 2. Tested with all the 4 Build configuration.
    fb5ed434  removed extra xml file
    98ae8d2f - Updates. 1. Updated README.md 2. added message on uart0 to look to uart1.
    265acb23  updated lastest platform_config_ref_src and removed spinlock as it was not needed in this example
    a793095f  updated README.md
    15a02e00 - cihanged application uart from mmuart0 to mmuart1. - updated project with latest [ mpfs_hal ](   https://bitbucket.microchip.com/projects/FPGA_PFSOC_ES/repos/mss-mpfs-hal-src/commits/d4ee8af4b011ef188ad458bc07f19f3cd433d841   ) **commit no** -: *d4ee8af4b01*. - Changed name of built in configurations as follow.  - **LIM-Debug**  download to and debug from LIM memory. Not-optimized (-O0). (Could be used with boot mode 0)  - **LIM-Release** download to and debug from LIM memory. Optimized    (-Os). (Could be used with boot mode 2)  - **DDR-Release** Execute from cached DDR memory – typically via a bootloader. Optimized (-Os)  - **eNVM-Scratchpad-Release** Booting from eNVM, program relocates itself to scratchpad memory and continues execution. Optimized (-Os). (Could be used with boot mode 1)
    23a0b82d  deleted lim-debug folder
    8627817b  updated README.txt to README.md
    8b097615  added attach and debug configurations
    c6be66c0  Updates-:
    
    git-subtree-dir: driver-examples/mss-gpio
    git-subtree-split: 0a262f453f1fb4881c6046376ea5eb6b0bdf93c2

--- mpfs-gpio-interrupt/src/application/hart1/u54_1.c
+++ mpfs-gpio-interrupt/src/application/hart1/u54_1.c
@@ -1,76 +1,228 @@
 /*******************************************************************************
- * Copyright 2019-2020 Microchip FPGA Embedded Systems Solutions.
+ * Copyright 2019-2021 Microchip FPGA Embedded Systems Solutions.
  *
  * SPDX-License-Identifier: MIT
  *
  * Application code running on U54_1
  *
+ * PolarFire SoC MSS GPIO interrupt example project
  */
 
 #include <stdio.h>
 #include <string.h>
 #include "mpfs_hal/mss_hal.h"
-#include "drivers/mss_mmuart/mss_uart.h"
+#include "drivers/mss/mss_gpio/mss_gpio.h"
+#include "drivers/mss/mss_mmuart/mss_uart.h"
 
-volatile uint32_t count_sw_ints_h1 = 0U;
+/******************************************************************************
+ * Instruction message. These message will be displayed on the UART terminal 
+   when the program starts.
+ *****************************************************************************/
 
-extern uint64_t uart_lock;
 
-/* Main function for the hart1(U54_1 processor).
- * Application code running on hart1 is placed here
- *
- * The hart1 goes into WFI. hart0 brings it out of WFI when it raises the first
- * Software interrupt to this hart.
+uint8_t g_message2[] =
+"\r\n\r\n\r\n **** PolarFire SoC MSS GPIO example ****\r\n\r\n\r\n\
+This program is running on u54_1.\r\n\r\n\
+Observe the LEDs blinking. LEDs toggle every time the SYSTICK timer expires\r\n\
+\r\n\
+Press 1 to generate interrupt on GPIO2 pin 30.\r\n\
+Press 2 to generate interrupt on GPIO2 pin 31.\r\n\
+Press 3 to generate interrupt on F2M_0 signal.\r\n";
+
+#define RX_BUFF_SIZE    64U
+
+uint8_t g_rx_buff[RX_BUFF_SIZE] = {0};
+volatile uint8_t g_rx_size = 0U;
+
+/* Main function for the hart1(U54 processor).
+ * Application code running on hart1 is placed here.
+ * On Icicle kit, apart from the UART menu, you can also use push button
+ * switches to generate GPIO interrupts. The mapping is as follows
+ * push button SW1 - MSS_INT_F2M[0]
+ * push button SW2 - GPIO2_30
+ * push button SW3 - GPIO2_31
  */
+
 void u54_1(void)
 {
-    uint8_t info_string[100];
+    uint64_t mcycle_start = 0U;
+    uint64_t mcycle_end = 0U;
+    uint64_t delta_mcycle = 0U;
     uint64_t hartid = read_csr(mhartid);
-    volatile uint32_t icount = 0U;
+    uint8_t cnt = 16U, int_num = 0U;
 
     /* Clear pending software interrupt in case there was any.
-       Enable only the software interrupt so that the E51 core can bring this
-       core out of WFI by raising a software interrupt. */
+     * Enable only the software interrupt so that the E51 core can bring this
+     * core out of WFI by raising a software interrupt In case of external,
+     * bootloader not present
+     */
+
     clear_soft_interrupt();
     set_csr(mie, MIP_MSIP);
 
-    /* Put this hart in WFI. */
+#if (IMAGE_LOADED_BY_BOOTLOADER == 0)
+
+    /*Put this hart into WFI.*/
+
     do
     {
         __asm("wfi");
     }while(0 == (read_csr(mip) & MIP_MSIP));
 
-    /* The hart is now out of WFI, clear the SW interrupt. Here onwards the
-     * application can enable and use any interrupts as required */
+    /* The hart is out of WFI, clear the SW interrupt. Hear onwards Application
+     * can enable and use any interrupts as required */
     clear_soft_interrupt();
+#endif
 
+    PLIC_init();
     __enable_irq();
 
-    mss_take_mutex((uint64_t)&uart_lock);
-    MSS_UART_polled_tx_string(&g_mss_uart0_lo,
-                              "Hello World from U54_1.\r\n");
-    mss_release_mutex((uint64_t)&uart_lock);
+    /* Reset the peripherals turn on the clocks */
 
-    while (1U)
+    (void)mss_config_clk_rst(MSS_PERIPH_MMUART1, (uint8_t) MPFS_HAL_FIRST_HART, PERIPHERAL_ON);
+    (void)mss_config_clk_rst(MSS_PERIPH_MMUART2, (uint8_t) MPFS_HAL_FIRST_HART, PERIPHERAL_ON);
+    (void)mss_config_clk_rst(MSS_PERIPH_MMUART3, (uint8_t) MPFS_HAL_FIRST_HART, PERIPHERAL_ON);
+    (void)mss_config_clk_rst(MSS_PERIPH_GPIO0, (uint8_t) MPFS_HAL_FIRST_HART, PERIPHERAL_ON);
+    (void)mss_config_clk_rst(MSS_PERIPH_GPIO1, (uint8_t) MPFS_HAL_FIRST_HART, PERIPHERAL_ON);
+    (void)mss_config_clk_rst(MSS_PERIPH_GPIO2, (uint8_t) MPFS_HAL_FIRST_HART, PERIPHERAL_ON);
+    (void)mss_config_clk_rst(MSS_PERIPH_CFM, (uint8_t) MPFS_HAL_FIRST_HART, PERIPHERAL_ON);
+
+   /* mmuart0 initialization */
+
+
+   /* mmuart1 initialization */
+
+    MSS_UART_init( &g_mss_uart1_lo,
+            MSS_UART_115200_BAUD,
+            MSS_UART_DATA_8_BITS | MSS_UART_NO_PARITY | MSS_UART_ONE_STOP_BIT);
+
+    MSS_UART_polled_tx_string(&g_mss_uart1_lo,
+                       g_message2 );
+
+    mcycle_start = readmcycle();
+
+    /* Configure Systick. The tick rate is configured in mss_sw_config.h */
+
+    SysTick_Config();
+
+    /* Making sure that the GPIO2 interrupts are routed to the PLIC instead of
+     * GPIO0 and GPIO1.
+     * Please see the mss_gpio.h for more description on how GPIO interrupts
+     * are routed to the PLIC */
+
+    SYSREG->GPIO_INTERRUPT_FAB_CR = 0xFFFFFFFFUL;
+
+    PLIC_SetPriority_Threshold(0);
+
+    for (int_num = 0u; int_num <= GPIO2_NON_DIRECT_PLIC; int_num++)
     {
-        icount++;
-        if (0x100000U == icount)
-        {
-            icount = 0U;
-            sprintf(info_string,"hart %d\r\n", hartid);
-            mss_take_mutex((uint64_t)&uart_lock);
-            MSS_UART_polled_tx(&g_mss_uart0_lo, info_string,
-                               strlen(info_string));
-            mss_release_mutex((uint64_t)&uart_lock);
-        }
+        PLIC_SetPriority(GPIO0_BIT0_or_GPIO2_BIT0_PLIC_0 + int_num, 2u);
     }
 
-    /* Never return */
+    MSS_GPIO_init(GPIO2_LO);
+
+    for (cnt = 16u; cnt< 20u; cnt++)
+    {
+        MSS_GPIO_config(GPIO2_LO,
+                        cnt,
+                        MSS_GPIO_OUTPUT_MODE);
+    }
+
+    MSS_GPIO_config(GPIO2_LO, MSS_GPIO_26, MSS_GPIO_OUTPUT_MODE);
+    MSS_GPIO_config(GPIO2_LO, MSS_GPIO_27, MSS_GPIO_OUTPUT_MODE);
+    MSS_GPIO_config(GPIO2_LO, MSS_GPIO_28, MSS_GPIO_OUTPUT_MODE);
+
+    MSS_GPIO_config(GPIO2_LO,
+                    MSS_GPIO_30,
+                    MSS_GPIO_INPUT_MODE | MSS_GPIO_IRQ_LEVEL_HIGH);
+
+    MSS_GPIO_config(GPIO2_LO,
+                    MSS_GPIO_31,
+                    MSS_GPIO_INPUT_MODE | MSS_GPIO_IRQ_LEVEL_HIGH);
+
+    MSS_GPIO_set_outputs(GPIO2_LO, 0u);
+
+    MSS_GPIO_enable_irq(GPIO2_LO, MSS_GPIO_30);
+    MSS_GPIO_enable_irq(GPIO2_LO, MSS_GPIO_31);
+    PLIC_SetPriority(FABRIC_F2H_0_PLIC, 2);
+    PLIC_EnableIRQ(FABRIC_F2H_0_PLIC);
+
+    while (1u)
+    {
+        g_rx_size = MSS_UART_get_rx(&g_mss_uart1_lo, g_rx_buff,
+                                            sizeof(g_rx_buff));
+
+        if (g_rx_size > 0u)
+        {
+            switch (g_rx_buff[0u])
+            {
+            case '1':
+                MSS_GPIO_set_output(GPIO2_LO, MSS_GPIO_26, 1u);
+                break;
+
+            case '2':
+                MSS_GPIO_set_output(GPIO2_LO, MSS_GPIO_27, 1u);
+                break;
+
+            case '3':
+                MSS_GPIO_set_output(GPIO2_LO, MSS_GPIO_28, 1);
+                break;
+
+            default:
+
+                /* Echo the characters received from the terminal */
+
+                MSS_UART_polled_tx_string(&g_mss_uart1_lo,
+                                   g_rx_buff );
+            }
+
+            g_rx_size = 0;
+        }
+    }
 }
 
-/* hart1 software interrupt handler */
-void Software_h1_IRQHandler(void)
+/* GPIO interrupt handlers */
+
+uint8_t  gpio1_bit16_or_gpio2_bit30_plic_30_IRQHandler(void)
 {
-    uint64_t hart_id = read_csr(mhartid);
-    count_sw_ints_h1++;
+    MSS_UART_polled_tx_string(&g_mss_uart1_lo,
+           "gpio2_pin 30 Interrupt\r\n");
+    MSS_GPIO_set_outputs(GPIO2_LO, 0u);
+    return EXT_IRQ_KEEP_ENABLED;
+}
+
+uint8_t gpio1_bit17_or_gpio2_bit31_plic_31_IRQHandler(void)
+{
+    MSS_UART_polled_tx_string(&g_mss_uart1_lo,
+           "gpio2_pin 31 Interrupt\r\n" );
+    MSS_GPIO_set_outputs(GPIO2_LO, 0u);
+    return EXT_IRQ_KEEP_ENABLED;
+}
+
+uint8_t fabric_f2h_0_plic_IRQHandler(void)
+{
+    MSS_UART_polled_tx_string(&g_mss_uart1_lo,
+               "f2h_0 interrupt\r\n" );
+    MSS_GPIO_set_outputs(GPIO2_LO, 0u);
+    return EXT_IRQ_KEEP_ENABLED;
+}
+
+void SysTick_Handler_h1_IRQHandler(void)
+{
+    uint32_t hart_id = read_csr(mhartid);
+    static volatile uint8_t value = 0u;
+
+    if (1u == hart_id)
+    {
+        if(0u == value)
+        {
+            value = 0x01u;
+        }
+        else
+        {
+            value = 0x00u;
+        }
+
+        MSS_GPIO_set_output(GPIO2_LO, MSS_GPIO_16, value);
+    }
 }
