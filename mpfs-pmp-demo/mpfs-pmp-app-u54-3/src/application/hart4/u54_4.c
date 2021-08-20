ffeff94d932e1dcbd9361e4477a25f5bedfc7965
Conor Paxton <conor.paxton@microchip.com>
2021-08-20 12:02:02 +0100

    Squashed 'applications/' changes from c4be30c..51f83ab
    51f83ab Adding mpfs-pmp-demo reference 2.0.1
    93e47ef Squashed 'mpfs-pmp-demo/' content from commit 47f2185
    
    git-subtree-dir: applications
    git-subtree-split: 51f83ab6d969bd0a941d31f96f906a114b61cdae

---
+++ mpfs-pmp-demo/mpfs-pmp-app-u54-3/src/application/hart4/u54_4.c
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ * Copyright 2019-2021 Microchip FPGA Embedded Systems Solution.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * MPFS Embedded Software example
+ * hart not used in this project
+ */
+/*******************************************************************************
+ *
+ * Code running on U54 hart 4
+ * hart not used in this project
+ *
+ */
+#include <stdio.h>
+#include <string.h>
+#include "mpfs_hal/mss_hal.h"
